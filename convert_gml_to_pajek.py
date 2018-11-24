b239afb1034e1eec45c92f97d6cc54f86aa7b067
Zwirek009 <maciek.wiraszka@gmail.com>
2018-11-24 19:10:29 +0100

    add reading .gml network

--- convert_gml_to_pajek.py
+++ convert_gml_to_pajek.py
@@ -1,7 +1,11 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 def main():
-    #TODO
+    G = nx.read_gml("network/lesmis.gml")
+    GU = G.to_undirected()
+    nx.draw(GU)
+    plt.show()
 
 if __name__ == "__main__":
     main()
